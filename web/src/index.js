53ca2c8a020ba93c0fa8ca356563138ff5d2ec00
xiaoshuai <1378334435@qq.com>
2022-02-27 21:35:32 +0800

    混合式开发

--- web/src/index.js
+++ web/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// ReactDOM.render(
+  ReactDOM.hydrate(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
